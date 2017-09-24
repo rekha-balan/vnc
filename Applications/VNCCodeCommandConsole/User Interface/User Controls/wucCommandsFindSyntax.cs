c16fc58e7eeb93c416e6f5548261df8ac51f98df
vncplace <vncplace@outlook.com>
2017-09-24 15:26:39 -0700

    Code Cleanup

--- Applications/VNCCodeCommandConsole/User Interface/User Controls/wucCommandsFindSyntax.cs
+++ Applications/VNCCodeCommandConsole/User Interface/User Controls/wucCommandsFindSyntax.cs
@@ -59,18 +59,6 @@ namespace VNCCodeCommandConsole.User_Interface.User_Controls
 
         #region Initialization
 
-        //private void LoadControlContents()
-        //{
-        //    //try
-        //    //{
-        //    //    wucFind_Picker.PopulateControlFromFile(Common.cCONFIG_FILE);
-        //    //}
-        //    //catch (Exception ex)
-        //    //{
-        //    //    MessageBox.Show(ex.ToString());
-        //    //}
-        //}
-
         internal override void OnLoaded(object sender, RoutedEventArgs e)
         {
 #if TRACE
@@ -156,13 +144,6 @@ namespace VNCCodeCommandConsole.User_Interface.User_Controls
         private void btnImportsStatementWalker_Click(object sender, RoutedEventArgs e)
         {
             ProcessOperation(DisplayImportsStatementWalkerVB);
-        }
-
-        private void btnHttpContextWalker_Click(object sender, RoutedEventArgs e)
-        {
-            var tag = ((System.Windows.Controls.Button)sender).Tag.ToString();
-
-            DisplayHttpContextWalkerVB(CodeExplorerContext, tag);
         }
 
         private void btnInvocationExpressionWalker_Click(object sender, RoutedEventArgs e)
@@ -365,112 +346,7 @@ namespace VNCCodeCommandConsole.User_Interface.User_Controls
                 tree, walker);
         }
 
-        private void DisplayHttpContextWalkerVB(wucCodeExplorerContext codeExplorerContext, string context)
-        {
-            StringBuilder sb = new StringBuilder();
-            Dictionary<string, Int32> matches = new Dictionary<string, Int32>();
-
-            var sourceCode = "";
-
-            // If a source file has been specified use it
-
-            if (codeExplorerContext.teSourceFile.Text.Length > 0)
-            {
-                string fileNameAndPath = codeExplorerContext.teSourceFile.Text;
-
-                if (!File.Exists(fileNameAndPath))
-                {
-                    sb.AppendLine(string.Format("File ({0}) does not exist.  Check path and name.", fileNameAndPath));
-                    goto PrematureExitBummer;
-                }
-
-                if ((Boolean)ceListImpactedFilesOnly.IsChecked)
-                {
-                    sb.AppendLine("Would Process these files ....");
-                    sb.AppendLine(string.Format("  {0}", fileNameAndPath));
-                }
-                else
-                {
-                    sb.AppendLine(string.Format("Processing ({0}) ...", fileNameAndPath));
-
-                    using (var sr = new StreamReader(fileNameAndPath))
-                    {
-                        sourceCode = sr.ReadToEnd();
-                    }
-
-                    var tree = VisualBasicSyntaxTree.ParseText(sourceCode);
-
-                    string pattern = string.Empty;
-
-                    VNC.CodeAnalysis.SyntaxWalkers.VB.HttpContextCurrentInvocationExpression walker = null;
-                    walker = new VNC.CodeAnalysis.SyntaxWalkers.VB.HttpContextCurrentInvocationExpression(context);
-                    walker.Messages = sb;
-                    walker.Matches = matches;
-
-                    walker.Visit(tree.GetRoot());
-                }
-            }
-            else
-            {
-                // Otherwise, go see if one or more files has been selected
-
-                if (codeExplorerContext.cbeSourceFile.SelectedItems.Count > 0)
-                {
-                    if ((Boolean)ceListImpactedFilesOnly.IsChecked)
-                    {
-                        sb.AppendLine("Would Process these files ....");
-                    }
-
-                    foreach (XElement file in codeExplorerContext.cbeSourceFile.SelectedItems)
-                    {
-                        string filePath = codeExplorerContext.teSourcePath.Text + wucCodeExplorerContext.GetFilePath(file);
-
-                        if ( ! File.Exists(filePath))
-                        {
-                            sb.AppendLine(string.Format("ERROR File ({0}) does not exist.  Check config file", filePath));
-                            continue;
-                        }
-
-                        if ((Boolean)ceListImpactedFilesOnly.IsChecked)
-                        {
-                            sb.AppendLine(string.Format("  {0}", filePath));
-                        }
-                        else
-                        {
-                            sb.AppendLine(string.Format("Processing ({0}) ...", filePath));
-
-                            using (var sr = new StreamReader(filePath))
-                            {
-                                sourceCode = sr.ReadToEnd();
-                            }
-
-                            var tree = VisualBasicSyntaxTree.ParseText(sourceCode);
-
-                            string pattern = string.Empty;
-
-                            VNC.CodeAnalysis.SyntaxWalkers.VB.HttpContextCurrentInvocationExpression walker = null;
-                            walker = new VNC.CodeAnalysis.SyntaxWalkers.VB.HttpContextCurrentInvocationExpression(context);
-                            walker.Messages = sb;
-                            walker.Matches = matches;
-
-                            walker.Visit(tree.GetRoot());
-                        }
-                    }
-                }
-            }
-
-            sb.AppendLine("Summary");
-
-            foreach (var item in matches.OrderBy(v => v.Key).Select(k => k.Key))
-            {
-                sb.AppendLine(string.Format("Count: {0,3} {1} ", matches[item], item));
-            }
-
- PrematureExitBummer:
-            CodeExplorer.teSourceCode.Text = sb.ToString();
-        }
-
-        #endregion
+         #endregion
 
         #region Utility Methods
 
