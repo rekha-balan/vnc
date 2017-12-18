0e70b1c54e96638adb16c743553434d1efbb41d8
crhodes <chrhodes@cox.net>
2017-12-18 06:35:51 -0800

    Added Pro C#

---
+++ Explore/C#/Pro C#6/Chapter_19/AsyncDelegate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Main() invoked on thread {0}.",
+              Thread.CurrentThread.ManagedThreadId);
+
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, null, null);
+
+            // If this check iftaR.IsCompleted is removed, the code will block
+            // on b.EndInvoke(iftAR);
+
+
+
+            //while (!iftAR.IsCompleted)
+            //{
+            //  This message will keep printing until
+            //  the Add() method is finished.
+
+            //    Console.WriteLine("Doing more work in Main()!");
+            //    Thread.Sleep(1000); 
+            //    // Sleep to avoid thousands of messages
+            //}
+
+            // Alternatively you can wait for a specified amount of time (ms)
+            // Note the Sleep has been removed
+
+            while (!iftAR.AsyncWaitHandle.WaitOne(2000, true))
+            {
+                Console.WriteLine("Doing more work in Main()!");
+            }
+
+            // Now we know the Add() method is complete.
+            // This call will block if Add is not finished.
+
+            int answer = b.EndInvoke(iftAR);
+
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            // Print out the ID of the executing thread.
+            Console.WriteLine("Add() invoked on thread {0}.",
+              Thread.CurrentThread.ManagedThreadId);
+
+            // Pause to simulate a lengthy operation.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
+
---
+++ Explore/C#/Pro C#6/Chapter_19/BackgroundThreads/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BackgroundThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Background Threads *****\n");
+
+            Printer p = new Printer();
+
+            Thread bgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+
+            // This is now a background thread.
+
+            // If you comment this out the app will keep running until
+            // the new foreground thread completes
+
+            //bgroundThread.IsBackground = true;
+            bgroundThread.Start();
+
+            Console.WriteLine("***** Background Thread Started *****\n");
+        }
+    }
+}
