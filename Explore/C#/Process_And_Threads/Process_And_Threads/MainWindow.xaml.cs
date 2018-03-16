9e0f550e73a4df2461ee8bffaca06bba1f7a9817
crhodes <chrhodes@cox.net>
2018-03-16 05:55:03 -0700

    Initial commit

---
+++ Explore/C#/Process_And_Threads/Process_And_Threads/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Process_And_Threads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+            var threads = currentProcess.Threads;
+
+            var processID = currentProcess.Id;
+
+            var currentThread = Thread.CurrentThread;
+            var currentContext = Thread.CurrentContext;
+
+            var appDomain = Thread.GetDomain();
+            var appDomainID = Thread.GetDomainID();
+        }
+    }
+}
