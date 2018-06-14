71f370b9a3386246ee025710200a3a11334aff12
crhodes <chrhodes@cox.net>
2018-06-13 19:51:36 -0700

    a

---
+++ Explore/Prisim/PrisimDemoSolution/Src/Modules/ModuleAS/ModuleSBNModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+using Infrastructure;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleSBN
+{
+    public class ModuleSBNModule : IModule
+    {
+        IUnityContainer _container;
+        IRegionManager _manager;
+
+        public ModuleSBNModule(IUnityContainer container, IRegionManager manager)
+        {
+            _container = container;
+            _manager = manager;
+        }
+
+        public void Initialize()
+        {
+            _container.RegisterType<IContentSBN, ContentSBN>();
+            _container.RegisterType<IContentSBNViewModel, ContentSBNViewModel>();
+
+            _manager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ContentSBN));
+        }
+    }
+}
