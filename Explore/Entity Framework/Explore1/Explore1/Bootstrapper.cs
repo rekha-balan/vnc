9ebb4db71497f280429c18a61b5b89e9ea172382
Christopher Rhodes <chrhodes@cox.net>
2018-06-14 17:43:07 -0700

    First steps

---
+++ Explore/Entity Framework/Explore1/Explore1/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Explore1.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Explore1
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
