be58140addfb25995ff4d6fc5bd5bad8c921c2e8
crhodes <chrhodes@cox.net>
2018-06-14 19:45:27 -0700

    a

---
+++ Explore/Entity Framework/Explore1/Ninja.DomainClasses/Classes.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NinjaDomain.Classes
+{
+    // These are all in same file so easier to see.
+
+    public class Ninja
+    {
+
+        #region Enums, Fields, Properties
+        public Clan Clan { get; set; }
+
+        public int ClanId { get; set; }
+
+        public System.DateTime DateOfBirth { get; set; }
+
+        public List<NinjaEquipment> EquipmentOwned { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool ServedInOniwaban { get; set; }
+        #endregion
+
+    }
+
+    public class Clan
+    {
+
+        #region Enums, Fields, Properties
+        public string ClanName { get; set; }
+
+        public int Id { get; set; }
+
+        public List<Ninja> Ninjas { get; set; }
+        #endregion
+
+    }
+
+    public class NinjaEquipment
+    {
+
+        #region Enums, Fields, Properties
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        public Ninja Ninja { get; set; }
+
+        public EquipmentType Type { get; set; }
+        #endregion
+
+    }
+}
---
+++ Explore/Entity Framework/Explore1/NinjaDomain.DataModel/NinjaContext.cs
@@ -0,0 +1,18 @@
+﻿using NinjaDomain.Classes;
+using System.Data.Entity;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext : DbContext
+    {
+
+        #region Enums, Fields, Properties
+        public DbSet<Clan> Clans { get; set; }
+
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+
+        public DbSet<Ninja> Ninjas { get; set; }
+        #endregion
+
+    }
+}
