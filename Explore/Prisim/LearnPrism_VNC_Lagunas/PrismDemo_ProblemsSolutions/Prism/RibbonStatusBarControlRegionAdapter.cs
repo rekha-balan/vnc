97c6f9055fbf5fd92bb3015142f04603b7eaaadc
Christopher Rhodes <chrhodes@cox.net>
2018-06-25 16:28:49 -0700

    Last try for today

--- Explore/Prisim/LearnPrism_VNC_Lagunas/PrismDemo_ProblemsSolutions/Prism/RibbonStatusBarControlRegionAdapter.cs
+++ Explore/Prisim/LearnPrism_VNC_Lagunas/PrismDemo_ProblemsSolutions/Prism/RibbonStatusBarControlRegionAdapter.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.Xpf.Ribbon;
+﻿using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Ribbon;
 using Prism.Regions;
 using System;
 using System.Collections.Specialized;
@@ -17,24 +18,24 @@ namespace PrismDemo.Prism
             if (region == null) throw new ArgumentNullException("region");
             if (regionTarget == null) throw new ArgumentNullException("regionTarget");
 
-            //region.Views.CollectionChanged += (s, e) =>
-            //{
-            //    if (e.Action == NotifyCollectionChangedAction.Add)
-            //    {
-            //        foreach (var view in e.NewItems)
-            //        {
-            //            AddViewToRegion(view, regionTarget);
-            //        }
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    foreach (var view in e.NewItems)
+                    {
+                        AddViewToRegion(view, regionTarget);
+                    }
 
-            //    }
-            //    else if (e.Action == NotifyCollectionChangedAction.Remove)
-            //    {
-            //        foreach (var view in e.OldItems)
-            //        {
-            //            RemoveViewFromRegion(view, regionTarget);
-            //        }
-            //    }
-            //};
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (var view in e.OldItems)
+                    {
+                        RemoveViewFromRegion(view, regionTarget);
+                    }
+                }
+            };
         }
 
         protected override IRegion CreateRegion()
@@ -42,18 +43,29 @@ namespace PrismDemo.Prism
             return new SingleActiveRegion();
         }
 
-        //static void AddViewToRegion(object view, XamRibbon xamRibbon)
-        //{
-        //    var ribbonTabItem = view as RibbonTabItem;
-        //    if (ribbonTabItem != null)
-        //        xamRibbon.Tabs.Add(ribbonTabItem);
-        //}
+        static void AddViewToRegion(object view, RibbonStatusBarControl statusBarControl)
+        {
+            var barItem = view as BarStaticItem;
+            if (barItem != null)
+            {
+                statusBarControl.LeftItems.Add(barItem);
+            }
 
-        //static void RemoveViewFromRegion(object view, XamRibbon xamRibbon)
-        //{
-        //    var ribbonTabItem = view as RibbonTabItem;
-        //    if (ribbonTabItem != null)
-        //        xamRibbon.Tabs.Remove(ribbonTabItem);
-        //}
+            //var ribbonTabItem = view as RibbonTabItem;
+            //if (ribbonTabItem != null)
+            //    xamRibbon.Tabs.Add(ribbonTabItem);
+        }
+
+        static void RemoveViewFromRegion(object view, RibbonStatusBarControl statusBarControl)
+        {
+            var barItem = view as BarStaticItem;
+            if (barItem != null)
+            {
+                statusBarControl.LeftItems.Remove(barItem);
+            }
+            //    var ribbonTabItem = view as RibbonTabItem;
+            //    if (ribbonTabItem != null)
+            //        xamRibbon.Tabs.Remove(ribbonTabItem);
+        }
     }
 }
