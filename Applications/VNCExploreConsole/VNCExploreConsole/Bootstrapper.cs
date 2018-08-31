8aaf86a746bf9613d920c8225dd12f9b3645e160
Christopher Rhodes <chrhodes@cox.net>
2018-08-31 16:35:43 -0700

    Hours of moving stuff around.

---
+++ Applications/VNCExploreConsole/VNCExploreConsole/Bootstrapper.cs
@@ -0,0 +1,69 @@
+﻿using VNCExploreConsole.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Infrastructure;
+using System.Windows.Controls;
+using Prism.Regions;
+using ModuleA;
+using PeopleViewer;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PersonRepository.CSV;
+using PersonRepository.SQL;
+
+namespace VNCExploreConsole
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+
+        // Step 1 - Configure the catalog of modules
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+
+            moduleCatalog.AddModule(typeof(ModuleAModule));
+            moduleCatalog.AddModule(typeof(PeopleViewerDIModule));
+            moduleCatalog.AddModule(typeof(PeopleViewerTightCouplingModule));
+            moduleCatalog.AddModule(typeof(PeopleViewerLooseCouplingModule));
+        }
+
+        // Step 2 - Configure the container
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            // Use the ServiceRepository
+            //Container.RegisterType<PersonRepository.Interface.IPersonRepository, ServiceRepository>();
+            // Use the CSVRepository
+            //Container.RegisterType<PersonRepository.Interface.IPersonRepository, CSVRepository>();
+            // Use the SQLRepository
+            Container.RegisterType<PersonRepository.Interface.IPersonRepository, SQLRepository>();
+        }
+
+        // Step 3 - Configure the RegionAdapters
+
+        protected override RegionAdapterMappings ConfigureRegionAdapterMappings()
+        {
+            RegionAdapterMappings mappings = base.ConfigureRegionAdapterMappings();
+            mappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+            return mappings;
+        }
+
+        // Step 4 - Create the Shell that will hold the modules in designated regions.
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        // Step 5 - Show the MainWindow
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ VisualStudio/VNCVSMenu/VNCVSMenu/FolderManagement.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace VNCVSMenu
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class FolderManagement
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("de4e8464-9efd-4ffd-bfef-08763ec8c4c0");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FolderManagement"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private FolderManagement(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.StartNotepad, menuCommandID);
+            //var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FolderManagement Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in FolderManagement's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new FolderManagement(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "FolderManagement";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void StartNotepad(object sender, EventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "notepad.exe";
+            proc.Start();
+        }
+    }
+}
